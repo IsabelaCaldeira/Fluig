03b30371fe7917738d9ad4fcc1487c61713c3caf
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-02-27 09:23:43 -0300

    funcionalizando dataset

--- novoPontoComercial/dataset/dsMoedas.js
+++ novoPontoComercial/dataset/dsMoedas.js
@@ -8,12 +8,13 @@ function createDataset(fields, constraints, sortfields){
 
     var ds = DatasetBuilder.newDataset();
 
-    ds.addcolumn("simbolo");
-    ds.addcolumn("nomeMoeda");
-
-    ds.addRow(new Array("R$","Real"));
-    ds.addRow(new Array("US$","Dólar Americano"));
-    ds.addRow(new Array("CAD","Dólar Canadense"));
+    ds.addColumn("simbolo");
+    ds.addColumn("nomeMoeda");
+    ds.addColumn("descCompleta");
+    
+    ds.addRow(new Array("R$","Real","R$ Real"));
+    ds.addRow(new Array("US$","Dólar Americano", "U$ Dólar Americano"));
+    ds.addRow(new Array("CAD","Dólar Canadense", "CAD descCompleta"));
 
     return ds;
 
