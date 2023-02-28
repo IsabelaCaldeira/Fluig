7db46bffb73f7dd668517626e0e1af2ca0a5c829
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-02-28 15:22:08 -0300

    analysing values

--- BPMECM_Advanced/scripts/comprarDolar0311_condicional.js
+++ BPMECM_Advanced/scripts/comprarDolar0311_condicional.js
@@ -2,6 +2,11 @@ function intermediateconditional2() {
 
     var valorDesejado = hAPI.getCardValue("valorDesejado");
 
-    var ds = DatasetFactory.getDataset("DS")
+    var ds = DatasetFactory.getDataset("dsValorDolar0311", null, null, null);
+    valorAtual = ds.getValue(0, "valorDolar");
+
+    if(valorAtual <= valorDesejado){
+        return true;
+    }
 
 }
