f8df739b3b2639ab9b1bbe8d3179e4b18199b2e6
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-04-11 08:59:08 -0300

    calling in a function

--- BPMECM_Advanced/form/bancoTalentos/avaliarCandidato.js
+++ BPMECM_Advanced/form/bancoTalentos/avaliarCandidato.js
@@ -1,28 +1,31 @@
 var xml;
 
-$.ajax({
-    async: false,
-    type: 'GET',
-    dataType: 'xml',
-    url: "xml/ECMWorkflowEngineService.xml",
-    sucess: function(ret){
-        console.log(ret);
-        xml = $(ret);
-    }
+$("#avaliarCandidato").click(function(){
+    $.ajax({
+        async: false,
+        type: 'GET',
+        dataType: 'xml',
+        url: "xml/ECMWorkflowEngineService.xml",
+        sucess: function(ret){
+            console.log(ret);
+            xml = $(ret);
+        }
+    });
+    
+    xml.find("[name=nome]").text($("nome").val());
+    xml.find("[name=email]").text($("email").val());
+    xml.find("[name=celular]").text($("celular").val());
+    xml.find("[name=cargo]").text($("cargo").val());
+    xml.find("[name=linkedin]").text($("linkedin").val());
+    
+    parent.WCMAPI.Create({
+        url: "/webdesk/ECMWorkflowEngineService?wsdl",
+        contentType: "text/xml",
+        dataType: "xml",
+        data: xml[0],
+        sucess: function(data){
+            console.log("Processo Iniciado");
+        }
+    });
 });
 
-xml.find("[name=nome]").text($("nome").val());
-xml.find("[name=email]").text($("email").val());
-xml.find("[name=celular]").text($("celular").val());
-xml.find("[name=cargo]").text($("cargo").val());
-xml.find("[name=linkedin]").text($("linkedin").val());
-
-parent.WCMAPI.Create({
-    url: "/webdesk/ECMWorkflowEngineService?wsdl",
-    contentType: "text/xml",
-    dataType: "xml",
-    data: xml[0],
-    sucess: function(data){
-        console.log("Processo Iniciado");
-    }
-})
