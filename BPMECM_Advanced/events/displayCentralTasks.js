083fbc955797e241dac84e2a237307e872879b68
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-04-12 09:16:01 -0300

    finishing display

--- BPMECM_Advanced/events/displayCentralTasks.js
+++ BPMECM_Advanced/events/displayCentralTasks.js
@@ -1,3 +1,5 @@
 function displayCentralTasks(links){
 
+    links.put("Domine a Central de Tarefas", "http://lab.fluig.com/portal/p/01/ecmnavigation?app_ecm_navigation_doc=316205:8082/portal/p/1/ecmnavigation?app_ecm_navigation_doc=54");
+
 }
